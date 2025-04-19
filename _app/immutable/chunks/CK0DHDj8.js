0616a6040003bc52a4b05686ee26b3c5d3aaac3e
MaartenVanDoninckPXL <116871178+MaartenVanDoninckPXL@users.noreply.github.com>
2025-04-19 14:19:45 +0000

    Deploying to gh-pages from @ MaartenVanDoninckPXL/portfolio-maarten-vandoninck@eae1032337f9b8663d9c4c55aed660363907ef69 🚀

---
+++ _app/immutable/chunks/CK0DHDj8.js
@@ -0,0 +1,68 @@
+import{t as C,a as I}from"./Bqf5hvaw.js";import{i as we}from"./DOmrJHRZ.js";import{p as je,s as c,m as k,g as e,l as Q,e as fe,c as g,r as v,a as ze,t as L,d as ye,b as a,n as D,h as Y}from"./BZj-yBpz.js";import{e as E}from"./LQZI1k7X.js";import{a as S,i as _e,s as Ie}from"./ClC6fr4p.js";import{g as H,j as F,h as ee,e as De,b as He,a as u,k as Ae,s as Ce,t,f as s,d as A,c as $e}from"./D3YOujRk.js";import{o as xe}from"./nd9Z18IV.js";import{f as Oe}from"./BviiYr3T.js";var Le=C('<div role="button" tabindex="0"><img class="piece-image svelte-1illbtr"></div>'),Ee=C('<div class="jigsaw-grid svelte-1illbtr"></div>'),Se=C(`<div class="completion-container svelte-1illbtr"><div class="revealed-content mt-8 svelte-1illbtr"><h2 class="mb-4 text-center text-2xl font-bold svelte-1illbtr">Studiereis</h2> <h3 class="mt-6 text-xl font-bold svelte-1illbtr">Omschrijving</h3> <p class="svelte-1illbtr">De studentenreis naar Linz en Salzburg was erop gericht om studenten kennis te laten maken
+					met een internationale IT-omgeving, waarbij zowel professionele als culturele aspecten
+					centraal stonden. Zo bezochten we de Fachhochschule Oberösterreich (FH OÖ) of University
+					of Applied Sciences Upper Austria in Hagenberg en het Ars Electronica Center in Linz,
+					terwijl we ook de stad Salzburg hebben bezocht om Oostenrijk verder te ontdekken. Mijn
+					doel voor deze reis was om IT in een ander land en een andere cultuur te ontdekken.</p> <div class="image-container svelte-1illbtr"><img alt="PXL studenten bij het Ars Electronica Center" class="content-image svelte-1illbtr"> <p class="image-caption svelte-1illbtr">PXL studenten bij het Ars Electronica Center in Linz</p></div> <h3 class="mt-6 text-xl font-bold svelte-1illbtr">Kern</h3> <p class="svelte-1illbtr">De studentenreis begon vroeg op woensdagochtend 19 maart, toen we in Hasselt opstapten
+					voor een lange busrit richting Linz. Na een dag op de weg kwamen we 's avonds aan in het
+					Sommerhaus Hotel, gelegen in een studentenwijk op zo'n vijf kilometer van het
+					stadscentrum. Deze eerste avond konden we vrij invullen, voor ons bestond dit uit samen
+					een plek zoeken om avondeten te eten en rustig de avond eindigen in het hotelcafé met wat
+					kaarten.</p> <p class="svelte-1illbtr">De volgende dag, donderdag 20 maart, stond geheel in het teken van het verkennen van Linz,
+					met bijzondere aandacht voor het Ars Electronica Center (AEC). We begonnen de dag met een
+					gezamenlijk ontbijt in ons hotel en vertrokken rond negen uur naar het stadscentrum. De
+					ochtend konden we vrij besteden aan het verkennen van de stad, waar wij een prachtige
+					historische omgeving omgeven door adembenemende natuur ontdekten. Om kwart voor twaalf
+					verzamelden wij ons bij het AEC voor een begeleide rondleiding.</p> <div class="image-container svelte-1illbtr"><img alt="Ars Electronica Center" class="content-image svelte-1illbtr"> <p class="image-caption svelte-1illbtr">Ars Electronica Center in Linz</p></div> <p class="svelte-1illbtr">Tijdens deze rondleiding werden wij ondergedompeld in een wereld waarin technologie en
+					samenleving op een indrukwekkende en originele manier tentoongesteld werden. De
+					interactieve installaties boden een uitstekende demonstratie van hoe Virtual en Augmented
+					Reality, robotica en creatieve wetenschap elkaar kunnen aanvullen. Deze middag bood mij
+					een nieuw perspectief op de rol van technologie, deze is niet enkel praktisch, maar kan
+					ook artistieke en sociale dimensies omvatten. Na afloop van de rondleiding hadden we de
+					mogelijkheid om langer te blijven om de diverse tentoonstellingen verder te verkennen of
+					terug te keren naar de binnenstad. Dit bood ons een waardevolle gelegenheid om het museum
+					verder te ontdekken. 's Avonds hadden we opnieuw vrije tijd en konden we lokale
+					eetgelegenheden bezoeken om samen de avond af te sluiten.</p> <p class="svelte-1illbtr">Op vrijdag 21 maart richtten we onze blik op het FH OÖ in Hagenberg. Na het ontbijt
+					vertrokken we opnieuw met de bus en kregen we bij aankomst verschillende presentaties,
+					onder meer over de studie- en onderzoekmogelijkheden en het IM.ma-programma (Interactive
+					Media). Er volgde ook een toelichting over de internationale context voor studenten die
+					interesse hebben in een uitwisselingsproject. De voormiddag werd afgesloten met een
+					campusrondleiding, waarbij duidelijk werd hoeveel aandacht er is voor praktijklokalen,
+					labs en projectruimtes.</p> <div class="image-container svelte-1illbtr"><img alt="Fachhochschule Oberösterreich" class="content-image svelte-1illbtr"> <p class="image-caption svelte-1illbtr">Fachhochschule Oberösterreich (FH OÖ) in Hagenberg</p></div> <p class="svelte-1illbtr">In de namiddag konden we deelnemen aan een workshop. Deze was "Computer Graphics and
+					Visualization," waar we samen met een lokale docent/student een applicatie tot stand
+					brachten met behulp van AI. Daarna konden we nog verder de tentoonstelling en standjes van
+					de lokale studenten verkennen. Dit gaf een beeld dat er veel gelijkenissen zijn tussen
+					onze manieren van werken, maar toch enkele interessante verschillen.</p> <p class="svelte-1illbtr">Zaterdag 22 maart bestond uit een culturele uitstap naar Salzburg. Na een rit van enkele
+					uren bereikten we rond de middag het centrum van Salzburg, waar we de rest van de dag vrij
+					konden invullen. Hier konden we een prachtige historische stad verkennen. Het was
+					onmogelijk om alles te verkennen, aangezien het de 2e grootste stad van Oostenrijk is,
+					maar wat we konden zien was prachtig. Het contrast tussen de hypermoderne sfeer in Linz en
+					het historisch erfgoed van Salzburg kon niet groter zijn, wat de ervaring extra bijzonder
+					maakte. We proefden lokale specialiteiten en verkenden bezienswaardigheden zoals de
+					Hohensalzburg-fortress en de oever van de Salzach-rivier.</p> <div class="image-container svelte-1illbtr"><img alt="Salzburg stad" class="content-image svelte-1illbtr"> <p class="image-caption svelte-1illbtr">Historisch centrum van Salzburg</p></div> <p class="svelte-1illbtr">Op zondag 23 maart keerden we na een vroeg ontbijt terug naar Hasselt. De terugrit was een
+					stuk rustiger na een vermoeiende reis, maar toch had je het gevoel dat je de personen die
+					om je heen zaten net een beetje beter kenden.</p> <h3 class="mt-6 text-xl font-bold svelte-1illbtr">Reflectie</h3> <p class="svelte-1illbtr">Tijdens deze studiereis heb ik niet alleen nieuwe dingen geleerd, maar ook mijn eigen
+					vaardigheden en interesses beter leren kennen. Ik merkte bijvoorbeeld dat taal soms een
+					barrière kon vormen, in Linz en Hagenberg sprak men wel Engels, maar alle tekst is
+					voorzien voor personen die Duits spreken. Dit dwong me om mijn beperkte kennis van het
+					Duits toch eens te benutten of om online tools te gebruiken, dit was natuurlijk niet
+					altijd even handig.</p> <p class="svelte-1illbtr">Bij de rondleidingen en workshops in Hagenberg zag ik hoe onze opleiding Toegepaste
+					Informatica sterk aansluit bij wat de FH OÖ aanbiedt. We hebben een vergelijkbare focus op
+					praktijkopdrachten en projectwerk. De verschillen zitten in de gespecialiseerde modules,
+					zoals Interactive Media, die een andere insteek hadden dan ik gewend ben. Dit vond ik
+					bijzonder interessant, omdat het me liet zien dat je met je IT-skills in diverse
+					richtingen kunt evolueren, zowel technisch als creatief.</p> <p class="svelte-1illbtr">Terwijl een situatie waar samenwerking niet direct nodig was zich voordeed, is het toch
+					belangrijk om een team te zijn, dit maakt namelijk de reis een veel aangenamere en
+					leerzamere ervaring. Achteraf kreeg je dan ook het gevoel dat je elkaar beter begreep en
+					kende, wat toekomstige projecten en ervaringen een stuk makkelijker gaat maken.</p> <p class="svelte-1illbtr">Ik koos voor deze opdracht in mijn portfolio omdat het een mooie mix is van internationale
+					ervaring, culturele uitwisseling en professionele oriëntatie. Ik heb ervaren hoe
+					verrijkend het is om in een internationale context. Deze reis was voor mij dus een groot
+					succes en ik zou het zeker opnieuw doen als ik de kans kreeg.</p></div></div>`),Fe=C('<div class="game-wrapper svelte-1illbtr"><!></div>');function Ge(ne,te){je(te,!1);const[$,ae]=Ie(),x=()=>S(F,"$jigsawCompleted",$),ie=()=>S(ee,"$jigsawInitialized",$),re=()=>S(H,"$jigsawPieces",$),P=k();let T=4,b=6,M=[],r=k([]),U=0,w=k(!1),se=k(!1);const oe=H.subscribe(n=>{n&&n.length>0&&c(r,n)});xe(()=>(F.set(!1),ie()?c(r,re()):(M=Array.from({length:T*b},(n,i)=>{const o=Math.floor(i/b)+1,m=i%b+1,d=`.//jigsaw/row-${o}-column-${m}.jpg`;return{id:i,correctIndex:i,imageUrl:d}}),c(r,[...M].sort(()=>Math.random()-.5)),H.set(e(r)),ee.set(!0)),J(),()=>{oe()}));function le(n,i){(o=>(Y(r,e(r)[n]=o[0]),Y(r,e(r)[i]=o[1])))([e(r)[i],e(r)[n]]),c(r,e(r)),H.set(e(r)),J()}function J(){e(r).every((i,o)=>i.correctIndex===o)&&F.set(!0)}let h=k(null);function de(n){e(h)===null?c(h,n):(e(h)!==n&&le(e(h),n),c(h,null))}function ce(){U++,U===T*b&&c(w,!0)}function ve(n){console.error(`Failed to load image: ${n}`)}Q(()=>(x(),e(w)),()=>{x()&&e(w)&&setTimeout(()=>{c(se,!0)},300)}),Q(()=>{},()=>{c(P,`
+        display: grid; 
+        grid-template-columns: repeat(${b}, minmax(0, 1fr));
+        border: 2px solid #9ca3af;
+        padding: 0.25rem;
+        width: 100%;
+        max-width: 800px;
+        margin: 0 auto;
+    `)}),fe(),we();var O=Fe(),ge=g(O);{var me=n=>{var i=Ee();De(i,15,()=>e(r),o=>o.id,(o,m,d)=>{var l=Le();let j;var p=g(l);v(l),L(f=>{j=He(l,1,"puzzle-piece svelte-1illbtr",null,j,f),u(l,"aria-label",`Jigsaw piece ${e(d)+1}`),u(p,"src",e(m).imageUrl),u(p,"alt",`Jigsaw piece ${e(d)+1}`)},[()=>({selected:e(h)===e(d)})],ye),E("load",p,ce),E("error",p,()=>ve(e(m).imageUrl)),Ae(l,()=>Oe,()=>({duration:150})),E("click",l,()=>de(e(d))),I(o,l)}),v(i),L(()=>Ce(i,e(P))),I(n,i)},pe=n=>{var i=Se(),o=g(i),m=a(g(o),2),d=a(m,2),l=a(d,2),j=g(l);D(2),v(l);var p=a(l,2),f=a(p,2),N=a(f,2),z=a(N,2),ue=g(z);D(2),v(z);var W=a(z,2),X=a(W,2),y=a(X,2),he=g(y);D(2),v(y);var G=a(y,2),R=a(G,2),_=a(R,2),be=g(_);D(2),v(_);var V=a(_,2),Z=a(V,2),q=a(Z,2),B=a(q,2),K=a(B,2),ke=a(K,2);v(o),v(i),L(()=>{u(j,"src",".//studiereis/PLX_AEC-min.jpg"),u(ue,"src",".//studiereis/AEC.webp"),u(he,"src",".//studiereis/FHOO.jpg"),u(be,"src",".//studiereis/Salzburg.jpg")}),t(1,m,()=>s,()=>({y:20,duration:500,delay:400})),t(1,d,()=>s,()=>({y:20,duration:500,delay:800})),t(1,l,()=>A,()=>({duration:600,delay:800,start:.9})),t(1,p,()=>s,()=>({y:20,duration:500,delay:800})),t(1,f,()=>s,()=>({y:20,duration:500,delay:800})),t(1,N,()=>s,()=>({y:20,duration:500,delay:800})),t(1,z,()=>A,()=>({duration:600,delay:800,start:.9})),t(1,W,()=>s,()=>({y:20,duration:500,delay:800})),t(1,X,()=>s,()=>({y:20,duration:500,delay:800})),t(1,y,()=>A,()=>({duration:600,delay:800,start:.9})),t(1,G,()=>s,()=>({y:20,duration:500,delay:800})),t(1,R,()=>s,()=>({y:20,duration:500,delay:800})),t(1,_,()=>A,()=>({duration:600,delay:800,start:.9})),t(1,V,()=>s,()=>({y:20,duration:500,delay:800})),t(1,Z,()=>s,()=>({y:20,duration:500,delay:800})),t(1,q,()=>s,()=>({y:20,duration:500,delay:800})),t(1,B,()=>s,()=>({y:20,duration:500,delay:800})),t(1,K,()=>s,()=>({y:20,duration:500,delay:800})),t(1,ke,()=>s,()=>({y:20,duration:500,delay:800})),t(1,o,()=>$e,()=>({duration:800,delay:300})),I(n,i)};_e(ge,n=>{!x()||!e(w)?n(me):n(pe,!1)})}v(O),I(ne,O),ze(),ae()}export{Ge as J};
---
+++ _app/immutable/entry/start.1YvTDu9R.js
@@ -0,0 +1,1 @@
+import{l as o,a as r}from"../chunks/dQ1f10If.js";export{o as load_css,r as start};
