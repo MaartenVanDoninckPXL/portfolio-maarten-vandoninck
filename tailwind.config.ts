6febb7dce864e913c73c5c0280b5f8ec789e7f3f
MaartenVanDoninckHydrofixDrinks <maarten.van.doninck@hydrofixdrinks.eu>
2025-04-19 15:20:12 +0200

    Add initial Tailwind CSS configuration

---
+++ tailwind.config.ts
@@ -0,0 +1,10 @@
+module.exports = {
+    content: [
+      './src/**/*.{html,js,svelte,ts}',
+      './static/**/*.{html,js}'
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
